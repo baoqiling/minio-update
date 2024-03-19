b9f6b1f2208d733d1051fb32207683c40624ea62
包琦玲 <unicorn95bao@163.com>
2024-03-19 09:01:04 +0800

    [add] 新加6.0/地铁安装包上传接口

--- src/main/java/net/bhxz/controller/MinioController.java
+++ src/main/java/net/bhxz/controller/MinioController.java
@@ -42,8 +42,11 @@ public class MinioController {
 
 //            "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=d5c520d6-89fd-464e-830a-17e3dd212662";
 
-    private final String fileNameCenter7 ="center7.txt";
     private final String propertiesCenter7 ="center7.properties";
+
+    private final String propertiesCenter6 ="center6.properties";
+
+    private final String propertiesSubway ="subway.properties";
 
     @Autowired
     public MinioController(MinioClient minioClient, RestTemplate restTemplate) {
@@ -95,6 +98,140 @@ public class MinioController {
 //            BHUtils.bucketExistsAndCreate(bucketName, minioClient);
 
             String name = "center7/" + folder + "/" + file.getName();
+            PutObjectArgs args = PutObjectArgs.builder()
+                    .bucket(bucketName)
+                    .object(name)
+                    .stream(inputStream, file.length(), -1)
+                    .contentType(contentType.split(";")[0])
+                    .build();
+
+            // 执行上传操作
+            minioClient.putObject(args);
+            IOUtils.closeQuietly(inputStream);
+
+            String message = folder + "版本的安装包已上传至 http://package.bhxz.host:9000 文件管理系统，文件路径为:" + name;
+
+            String s = BHUtils.pushWebhookBotNotify(restTemplate, webhookUrlCenter7, message);
+            log.error(s);
+            log.error("File uploaded successfully.");
+            return ResponseEntity.ok("File uploaded successfully.");
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.error("Error uploading file: " + e.getMessage());
+            return ResponseEntity.status(500).body("Error uploading file: " + e.getMessage());
+        }
+    }
+
+    @PostMapping("/center6/uploadFile")
+    @ApiOperation(value = "6.0安装包上传接口")
+    public ResponseEntity<String> uploadCenter6Package() {
+        String fileUrl = "D:\\package\\center6\\BSServerSetup.exe";
+        String folder = "6.0.0.17";
+        BHUtils.createFile(propertiesCenter6);
+        String propertiesStr = BHUtils.readFile(propertiesCenter6);
+        Properties properties = new Properties();
+        InputStream input = null;
+        OutputStream output = null;
+        try {
+            if (StringUtils.isNotBlank(propertiesStr)){
+                input = new FileInputStream(propertiesCenter6);
+                properties.load(input);
+                fileUrl = properties.getProperty("fileUrl");
+                folder = properties.getProperty("version");
+                String newVersion = BHUtils.incrementVersion(folder);
+                properties.setProperty("version", newVersion);
+                output = new FileOutputStream(propertiesCenter6);
+                properties.store(output,"update version");
+            }else {
+                properties.setProperty("fileUrl","D:\\package\\center6\\BSServerSetup.exe");
+                properties.setProperty("version", "6.0.0.17");
+                output = new FileOutputStream(propertiesCenter6);
+                properties.store(output, "init properties");
+            }
+        } catch (IOException e) {
+            log.error("未读取到配置文件信息：" + e.getMessage());
+            throw new RuntimeException(e);
+        }
+
+        File file = new File(fileUrl);
+        try (
+                InputStream inputStream = new FileInputStream(file)
+        ) {
+            Tika tika = new Tika();
+            String contentType = tika.detect(file);
+            // 设置存储桶名称
+            String bucketName = "package";
+
+//            BHUtils.bucketExistsAndCreate(bucketName, minioClient);
+
+            String name = "center6/" + folder + "/" + file.getName();
+            PutObjectArgs args = PutObjectArgs.builder()
+                    .bucket(bucketName)
+                    .object(name)
+                    .stream(inputStream, file.length(), -1)
+                    .contentType(contentType.split(";")[0])
+                    .build();
+
+            // 执行上传操作
+            minioClient.putObject(args);
+            IOUtils.closeQuietly(inputStream);
+
+            String message = folder + "版本的安装包已上传至 http://package.bhxz.host:9000 文件管理系统，文件路径为:" + name;
+
+            String s = BHUtils.pushWebhookBotNotify(restTemplate, webhookUrlCenter7, message);
+            log.error(s);
+            log.error("File uploaded successfully.");
+            return ResponseEntity.ok("File uploaded successfully.");
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.error("Error uploading file: " + e.getMessage());
+            return ResponseEntity.status(500).body("Error uploading file: " + e.getMessage());
+        }
+    }
+
+    @PostMapping("/subway/uploadFile")
+    @ApiOperation(value = "地铁安装包上传接口")
+    public ResponseEntity<String> uploadSubwayPackage() {
+        String fileUrl = "D:\\package\\subway\\BSServerSetup.exe";
+        String folder = "6.1.0.19";
+        BHUtils.createFile(propertiesSubway);
+        String propertiesStr = BHUtils.readFile(propertiesSubway);
+        Properties properties = new Properties();
+        InputStream input = null;
+        OutputStream output = null;
+        try {
+            if (StringUtils.isNotBlank(propertiesStr)){
+                input = new FileInputStream(propertiesSubway);
+                properties.load(input);
+                fileUrl = properties.getProperty("fileUrl");
+                folder = properties.getProperty("version");
+                String newVersion = BHUtils.incrementVersion(folder);
+                properties.setProperty("version", newVersion);
+                output = new FileOutputStream(propertiesSubway);
+                properties.store(output,"update version");
+            }else {
+                properties.setProperty("fileUrl","D:\\package\\subway\\BSServerSetup.exe");
+                properties.setProperty("version", "6.1.0.19");
+                output = new FileOutputStream(propertiesSubway);
+                properties.store(output, "init properties");
+            }
+        } catch (IOException e) {
+            log.error("未读取到配置文件信息：" + e.getMessage());
+            throw new RuntimeException(e);
+        }
+
+        File file = new File(fileUrl);
+        try (
+                InputStream inputStream = new FileInputStream(file)
+        ) {
+            Tika tika = new Tika();
+            String contentType = tika.detect(file);
+            // 设置存储桶名称
+            String bucketName = "package";
+
+//            BHUtils.bucketExistsAndCreate(bucketName, minioClient);
+
+            String name = "subway/" + folder + "/" + file.getName();
             PutObjectArgs args = PutObjectArgs.builder()
                     .bucket(bucketName)
                     .object(name)
